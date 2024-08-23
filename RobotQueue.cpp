f76bae9f44400896a33c477b2633d655af150075
bob <hoanganhan04@gmail.com>
2024-08-23 10:57:36 +0700

    Refactor RobotStack.cpp for improved readability and performance

--- RobotQueue.cpp
+++ RobotQueue.cpp
@@ -5,14 +5,14 @@ typedef pair<int, int> toado;
 #define x first
 #define y second
 int main(){
-    stack<toado> S;
+    queue<toado> S;
     set<toado> Res;
     int x,y;
     cin >> x >> y;
     S.push({x,y});
     Res.insert({x,y});
     while (S.size()){
-        int u=S.top().first, v=S.top().second;
+        int u=S.front().first, v=S.front().second;
         S.pop();
         if(u%2==0 && Res.find({v,u/2})==Res.end()) {S.push({v,u/2}); Res.insert({v,u/2});}
         if(v%2!=0 && Res.find({v/2+1,u})==Res.end()){ S.push({v/2+1,u}); Res.insert({v/2+1,u});
